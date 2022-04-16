a8f0d4b93fcaa7f5a3ff71cd91c67f5e910d258c
evgeniy <br4tkov@yandex.com>
2022-04-16 17:44:27 +0300

    Добавляет футер

---
+++ source/js/modules/accordeon.js
@@ -0,0 +1,14 @@
+document.querySelector('.footer__nav').classList.remove('accordeon--open');
+
+document.querySelectorAll('.accordeon__trigger').forEach((accordeonTrigger) => {
+  accordeonTrigger.addEventListener('click', () => {
+    const accordeonParent = accordeonTrigger.parentNode;
+
+    if (accordeonParent.classList.contains('accordeon--open')) {
+      accordeonParent.classList.remove('accordeon--open')
+    } else {
+      document.querySelectorAll('.accordeon').forEach((accordeon) => accordeon.classList.remove('accordeon--open'))
+      accordeonParent.classList.add('accordeon--open');
+    }
+  })
+})
